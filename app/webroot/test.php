9bf0fab5347789b6c05c8207b58219926900ce78
Nerijus Eimanavičius <nermnv@telesoftas.com>
2014-08-28 15:37:35 +0300

    For index.php and test.php everything is relative to the application root now.

--- app/webroot/test.php
+++ app/webroot/test.php
@@ -19,6 +19,10 @@
 set_time_limit(0);
 ini_set('display_errors', 1);
 
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
 chdir(dirname(dirname(__FILE__)));
 
 /**
